a18b50afc9b44834f7d3bfe55b2720303812ec1e
Zhao Yifeng <41801956+VernZHYF@users.noreply.github.com>
2020-06-16 10:34:03 +0800

    Create main.py
    main codes

---
+++ main.py
@@ -0,0 +1,32 @@
+from averPlotRet import *
+from lineFitting import *
+from normFit import *
+
+# fixed parameter
+Dd = 0.01
+timeSteps = 80
+numCases = 16
+
+
+
+dataSets = dataExtr()
+stdList = []
+x = np.linspace(0, timeSteps, timeSteps)
+for i in range(timeSteps):
+    averageData = averPlotRet(dataSets[i], 0, numCases)
+    mean, std, amplitude = normFit(averageData)
+    stdList.append(std ** 2)
+
+fig4 = plt.figure(3)
+plt.plot(x, stdList)
+plt.title("stdSquare - t")
+plt.xlabel("t")
+plt.ylabel("Square of STD")
+
+
+D = lineFitting(x, stdList)
+
+with open("result.txt", "w") as f:
+    f.write("D = {}".format(D))
+
+plt.savefig("FittingCurve")
